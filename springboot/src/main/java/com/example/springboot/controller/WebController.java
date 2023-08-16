20fe259bdf259953580956b1195d2714fca2790d
xqnode <xqnode@163.com>
2023-08-16 22:41:05 +0800

    9. SpringBoot结合Mybatis实现数据的增删改查

--- springboot/src/main/java/com/example/springboot/controller/WebController.java
+++ springboot/src/main/java/com/example/springboot/controller/WebController.java
@@ -1,8 +1,9 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.common.Result;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 功能：提供接口返回数据
@@ -10,11 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
  * 日期：2023/8/13 15:55
  */
 @RestController
+@RequestMapping("/web")
 public class WebController {
 
-    @RequestMapping
+    @GetMapping("/hello")   //  /web/hello
     public Result hello(String name) {
         return Result.success(name);
+    }
+
+    @PostMapping("/post")
+    public Result post(@RequestBody Obj obj) {
+        System.out.println(obj.getName() == null ?  "是null" : obj.getName().isEmpty());
+        return Result.success(obj);
+    }
+
+    @PutMapping("/put")     //
+    public Result put(@RequestBody Obj obj) {
+        return Result.success(obj);
+    }
+
+    @DeleteMapping("/delete")     //   http://localhost:9090/web/delete/1    /1  =>  /{id}
+    public Result delete(@RequestBody List<Integer> ids) {
+        return Result.success(ids);
     }
 
 }
