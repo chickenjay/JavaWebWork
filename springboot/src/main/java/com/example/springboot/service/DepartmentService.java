bfdc90b75863a2f4e706052f00de92a2597688f1
jayjay_lu <794834897@qqcom>
2023-11-20 17:01:40 +0800

    增加部门类

---
+++ springboot/src/main/java/com/example/springboot/service/DepartmentService.java
@@ -0,0 +1,17 @@
+package com.example.springboot.service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.springboot.entity.Department;
+import com.example.springboot.mapper.DepartmentMapper;
+import com.example.springboot.mapper.NoticeMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class DepartmentService extends ServiceImpl<DepartmentMapper, Department> {
+    @Resource
+    DepartmentMapper departmentMapper;
+
+
+}
